20276f578b171b3425fcfbbb339cfecc11b2f9d6
Mohamed Moustafa <mohamed.moustafa.unifi.solutions>
2025-09-30 22:39:25 +0300

    add env variables & confgi module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT') || 8080;
+
+  await app.listen(PORT, () =>
+    console.log(`server up and running on port ${PORT}`),
+  );
 }
 bootstrap();
